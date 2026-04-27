d972ebafa85892aee9edc8f6adfe6a0af0a21486
usrecnik <urh@srecnik.info>
2026-04-27 12:59:18 +0200

    tests updated

--- tests/test-fs.sh
+++ tests/test-fs.sh
@@ -2,8 +2,9 @@
 #
 
 G_TEST="${1:-/mnt/asm/+DATA}"
+G_TEMP="${2:-/mnt/ramdisk}"
 
-find "$G_TEST" -type f -exec ./test-file.sh {} \;
+find "$G_TEST" -type f -exec ./test-file.sh {} "$G_TEMP" \;
 
 echo "(all tests finished)"
 
